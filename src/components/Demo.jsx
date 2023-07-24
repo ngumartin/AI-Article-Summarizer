6ed43ac71b668712e293b820947e8de95e089c5e
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-07-24 14:18:08 -0700

    Completed and style input on demo page.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -25,12 +25,15 @@ const Demo = () => {
                     />
                     <button
                     type='submit'
-                    className='submit_btn'
+                    className='submit_btn
+                        peer-focus:border-gray-700
+                        peer-focus:text-gray-700
+                    '
                     >
                         ↵
-
                     </button>
                 </form>
+                {/* browse URL history */}
             </div>
         </section>
     )
