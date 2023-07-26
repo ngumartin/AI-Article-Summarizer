5725905060ea8daff4f758b6683d447cd8d41f51
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-07-25 19:32:03 -0700

    Set up store.js and article.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import App from './App.jsx'
+import App from './App';
+import { store } from './services/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <provider>
+    <provider store={ store }>
       <App />
     </provider>
   </React.StrictMode>,
-)
+);
--- src/services/store.js
+++ src/services/store.js
@@ -1,1 +1,10 @@
-import { congifgureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+
+import { articleApi } from './article';
+
+export const store = configureStore ({
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+});
